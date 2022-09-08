46a810012d38cb284bb48988f5bfbf46062ce02e
s <s>
2022-09-08 09:31:06 +0500

    base js code

--- 01 counter/src/App.js
+++ 01 counter/src/App.js
@@ -1,13 +1,21 @@
 import './index.scss';
 
 function App() {
+  let count = 0;
+
+  const onClickPlus = () => {
+    count++;
+  };
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
+        <h1>{count}</h1>
         <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <button onClick={onClickPlus} className="plus">
+          Плюс +
+        </button>
       </div>
     </div>
   );
