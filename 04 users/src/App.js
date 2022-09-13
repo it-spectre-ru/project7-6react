41ad622fae684efbc8b59a50849c0955c7c72e4f
s <s>
2022-09-13 20:22:38 +0500

    render plus or minus icon user if invited

--- 04 users/src/App.js
+++ 04 users/src/App.js
@@ -7,6 +7,7 @@ import { Users } from './components/Users';
 
 function App() {
   const [users, setUsers] = React.useState([]);
+  const [invites, setInvites] = React.useState([]);
   const [isLoading, setLoading] = React.useState(true);
   const [searchValue, setSearchValue] = React.useState('');
 
